139935a6911f9a1b2e631bbe958eda089d075a8c
Jace Browning <jacebrowning@gmail.com>
2017-12-05 20:09:31 -0500

    Fetch registration with 'refresh=true'

--- api/api/viewsets.py
+++ api/api/viewsets.py
@@ -1,3 +1,5 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import viewsets
 
 from api.voters.models import Status
@@ -9,3 +11,10 @@ class StatusViewSet(viewsets.ModelViewSet):
     queryset = Status.objects.all()
     serializer_class = serializers.StatusSerializer
     http_method_names = ['get']
+
+    def retrieve(self, request, pk=None):
+        if request.query_params.get('refresh'):
+            status = get_object_or_404(Status, pk=pk)
+            status.fetch_and_update_registration()
+
+        return super().retrieve(request, pk)
