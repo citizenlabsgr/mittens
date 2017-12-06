a47efa089cd2a44f5502c0c824a2aa258b05791d
Jace Browning <jacebrowning@gmail.com>
2017-12-05 19:10:53 -0500

    Add a utility to fetch registration status from the lambda API

---
+++ api/voters/helpers.py
@@ -0,0 +1,25 @@
+import logging
+
+import requests
+
+
+REGISTRATION_API = "https://4gw9vvs9j1.execute-api.us-east-2.amazonaws.com/prod/checkRegistration"
+
+
+def fetch_and_update_registration(status):
+    voter = status.voter
+    payload = {
+        'firstName': voter.first_name,
+        'lastName': voter.last_name,
+        'birthMonth': voter.birth_month,
+        'birthYear': voter.birth_year,
+        'zip': voter.zip_code,
+    }
+
+    response = requests.get(REGISTRATION_API, params=payload)
+    assert response.status_code == 200, response
+
+    data = response.json()
+    logging.info(f"Voter registration data: {data}")
+    status.registered = data['registered']
+    status.save()
