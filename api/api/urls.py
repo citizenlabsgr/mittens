819e43ffe3eb3a0138a47170a9bb095d24d683e1
Jace Browning <jacebrowning@gmail.com>
2017-12-06 19:41:59 -0500

    Cleanup up routes

--- api/api/urls.py
+++ api/api/urls.py
@@ -9,10 +9,9 @@ root = routers.DefaultRouter()
 
 root.register('registration', viewsets.RegistrationViewSet, base_name='registration')
 root.register('timelines', viewsets.TimelineViewSet)
-# root.register('registration', viewsets.RegistrationViewSet.as_view(), base_name='registration')
 
 urlpatterns = [
     path('', include(root.urls)),
-    # path('registration/', viewsets.RegistrationViewSet.as_view()),
+
     path('client/', include('rest_framework.urls')),
 ]
