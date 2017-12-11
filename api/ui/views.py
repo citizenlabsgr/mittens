92589d8da8c0bf30233623462a5f24dd5890ee0a
Jace Browning <jacebrowning@gmail.com>
2017-12-11 16:12:42 -0500

    Never cache the index

--- api/ui/views.py
+++ api/ui/views.py
@@ -1,6 +1,8 @@
 from django.conf import settings
 from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
 
 
+@never_cache
 def index(request):
     return serve(request, 'index.html', settings.STATIC_ROOT)
