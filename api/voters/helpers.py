b8629d68c3b201e4df9f5d43e96322489702eda8
Jace Browning <jacebrowning@gmail.com>
2017-12-19 20:06:54 -0500

    Log unknown county, cities, and wards

--- api/voters/helpers.py
+++ api/voters/helpers.py
@@ -3,6 +3,8 @@ import logging
 
 import requests
 
+from api.elections.models import County, City, Ward
+
 
 REGISTRATION_API = "https://4gw9vvs9j1.execute-api.us-east-2.amazonaws.com/prod/checkRegistration"
 
@@ -25,4 +27,36 @@ def fetch_and_update_registration(voter, status):
     data = response.json()
     formatted_data = pprint.pformat(data)
     log.info(f"Voter registration data:\n{formatted_data}")
+
+    _find_county(data)
+    _find_city(data)
+    _find_ward(data)
+
     status.registered = data['registered']
+
+
+def _find_county(data):
+    value = data['county']
+    name = value.replace(" County", "")
+    try:
+        County.objects.get(name=name)
+    except County.DoesNotExist:
+        log.error(f"No such county: {name} ({value})")
+
+
+def _find_city(data):
+    value = data['jurisdiction']
+    name = value.replace("City of ", "")
+    try:
+        City.objects.get(name=name)
+    except City.DoesNotExist:
+        log.error(f"No such city: {name} ({value})")
+
+
+def _find_ward(data):
+    value = data['ward']
+    name = value
+    try:
+        Ward.objects.get(name=name)
+    except Ward.DoesNotExist:
+        log.error(f"No such ward: {name} ({value})")
