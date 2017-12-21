926a34920b3a1cfe9c3586f4f4a841b8f5d6c7a3
Jace Browning <jacebrowning@gmail.com>
2017-12-21 18:55:19 -0500

    Store regions from Michigan SOS fetches

--- api/elections/models.py
+++ api/elections/models.py
@@ -1,30 +1,7 @@
 from django.db import models
-from django.core.exceptions import ValidationError
 
 
-class County(models.Model):
-
-    class Meta:
-        verbose_name_plural = "Counties"
-
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class City(models.Model):
-
-    class Meta:
-        verbose_name_plural = "Cities"
-
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
-class Ward(models.Model):
+class Kind(models.Model):
 
     name = models.CharField(max_length=50)
 
@@ -34,38 +11,17 @@ class Ward(models.Model):
 
 class Region(models.Model):
 
-    class Meta:
-        unique_together = ('county', 'city', 'ward')
-
-    county = models.ForeignKey(County, blank=True, null=True,
-                               on_delete=models.PROTECT)
-    city = models.ForeignKey(City, blank=True, null=True,
-                             on_delete=models.PROTECT)
-    ward = models.ForeignKey(Ward, blank=True, null=True,
-                             on_delete=models.PROTECT)
-
+    kind = models.ForeignKey(Kind, on_delete=models.PROTECT)
+    name = models.CharField(max_length=200)
+    # TODO: Store GeoJSON for each region?
     # geo = models.JSONField()
+    verified = models.NullBooleanField(default=None)
+
+    class Meta:
+        unique_together = ['kind', 'name']
 
     def __str__(self):
-        return self.name
-
-    @property
-    def name(self):
-        if self.county:
-            return f"{self.county} County"
-        if self.city and self.ward:
-            return f"City of {self.city} - Ward {self.ward}"
-        return f"City of {self.city}"
-
-    def clean(self):
-        super().clean()
-        msg = "Set one of the following: County, City, City + Ward"
-        if self.county and (self.city or self.ward):
-            raise ValidationError(msg)
-        if self.city and self.county:
-            raise ValidationError(msg)
-        if self.ward and not self.city:
-            raise ValidationError(msg)
+        return f"{self.kind}: {self.name}"
 
 
 class Election(models.Model):
