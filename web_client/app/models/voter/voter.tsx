dcca2cf463086b17d0f886ca7c8f678d106e3ca7
Patrick Sollars <pjsollars@gmail.com>
2018-01-16 20:51:20 -0500

    birth date to three inline inputs

--- web_client/app/models/voter/voter.tsx
+++ web_client/app/models/voter/voter.tsx
@@ -4,13 +4,13 @@ import { VoterService, IncomingRegistrationJSON } from './voter-service';
 export class Voter {
   @observable firstName: string
   @observable lastName: string
-  @observable birthDate: string
+  @observable birthDate: Date
   @observable zipCode: string
   @observable registered: boolean
 
   @action
   checkRegistration() {
-    return VoterService.checkRegistration(this.firstName, this.lastName, this.birthDate, this.zipCode).then(
+    return VoterService.checkRegistration(this.firstName, this.lastName, this.birthDate.toISOString(), this.zipCode).then(
       result => {
         Object.assign(this, result);
         return this.registered;
--- web_client/app/registration-check/registration-check.tsx
+++ web_client/app/registration-check/registration-check.tsx
@@ -6,6 +6,7 @@ import { go } from 'router';
 import { MainContentWrapper } from 'main-content-wrapper/main-content-wrapper';
 import { ShortInput } from 'forms/short-input/short-input';
 import { Button } from 'button/button';
+import { Labelled } from 'forms/labelled/labelled';
 
 // CSS
 import { styles, vars, css, centeredBox } from 'styles/css';
@@ -20,7 +21,9 @@ export class RegistrationCheck extends React.Component<RegistrationCheckProps, {
   state = {
     firstName: "",
     lastName: "",
-    birthDate: "",
+    birthDay: "",
+    birthMonth: "",
+    birthYear: "",
     zipCode: "",
     voter: null as Voter
   }
@@ -36,8 +39,9 @@ export class RegistrationCheck extends React.Component<RegistrationCheckProps, {
   }
 
   submit = () => {
-    const { voter, firstName, lastName, birthDate, zipCode } = this.state;
-    Object.assign(voter, { firstName, lastName, birthDate, zipCode });
+    const { voter, firstName, lastName, birthDay, birthMonth, birthYear, zipCode } = this.state;
+    
+    Object.assign(voter, { firstName, lastName, birthDay, birthMonth, birthYear, zipCode });
     voter.checkRegistration().then(r => {
       if (r) {
         go('/registration-verified');
@@ -55,9 +59,17 @@ export class RegistrationCheck extends React.Component<RegistrationCheckProps, {
             <h1 {...style.heading}>Are you registered?</h1>
             <ShortInput label="First Name" onChange={this.setter('firstName')} placeholder="Susan" value={this.state.firstName}/>
             <ShortInput label="Last Name" onChange={this.setter('lastName')} placeholder="Anthony" value={this.state.lastName}/>
-            <ShortInput label="Birthday" onChange={this.setter('birthDate')} value={this.state.birthDate} placeholder="YYYY-MM-DD" />
+            <Labelled label="Birth Date">
+              <div {...style.inline}>
+                <div {...style.monthInput}>
+                  <ShortInput label="" onChange={this.setter('birthMonth')} value={this.state.birthMonth} placeholder="Month" />
+                </div>
+                <ShortInput label="" onChange={this.setter('birthDay')} value={this.state.birthDay} placeholder="Day" />
+                <ShortInput label="" onChange={this.setter('birthYear')} value={this.state.birthYear} placeholder="Year" />
+              </div>
+            </Labelled>
             <ShortInput label="Zip Code" onChange={this.setter('zipCode')} value={this.state.zipCode}/>
-            <Button action={this.submit} css={style.button}> Find Me!</Button>
+            <Button action={this.submit} css={style.button}>Find Me!</Button>
           </div>
         </div>
       </MainContentWrapper>
@@ -76,22 +88,18 @@ const style = styles({
   heading: {
     marginBottom: vars.spacing
   },
+  inline: {
+    display: 'flex',
+    margin: `0px ${-vars.smallSpacing/2}px`,
+    ' label': {
+      margin: `0px ${vars.smallSpacing/2}px`
+    }
+  },
+  monthInput: {
+      minWidth: 120
+  },
   maxWidth: {
     maxWidth: 400,
     margin: '0 auto'
-  },
-  registered: {
-    borderRadius: vars.border.borderRadius,
-    marginTop: vars.spacing,
-    padding: vars.spacing,
-    backgroundColor: vars.color.theme,
-    color: vars.color.white
-  },
-  notRegistered: {
-    borderRadius: vars.border.borderRadius,
-    marginTop: vars.spacing,
-    padding: vars.spacing,
-    backgroundColor: vars.color.warn,
-    color: vars.color.white
   }
 });
