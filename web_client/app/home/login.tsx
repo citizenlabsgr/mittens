36484fe7dd8dc6ee9785ea71ad7556c1e297e9bb
Jace Browning <jacebrowning@gmail.com>
2018-01-17 21:17:46 -0500

    Redirect based on registration status

--- web_client/app/home/login.tsx
+++ web_client/app/home/login.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { observer } from 'mobx-react';
+import { go } from 'router';
 
 import { MainContentWrapper } from 'main-content-wrapper/main-content-wrapper';
 import { ShortInput } from 'forms/short-input/short-input';
@@ -17,11 +18,30 @@ export type LoginProps = {
 @observer
 export class Login extends React.Component<LoginProps, {}> {
   state = {
-    email: ""
+    email: "",
+    errors: {} as {
+      email: string[],
+    }
   }
 
   componentWillMount() {
     this.setState({});
+  }
+
+  componentDidMount() {
+    API.get('registration/').then(data => {
+      const registered = data['registered'];
+      console.log("Registered: " + registered);
+      if (registered === true) {
+        go('/registration-verified');
+      } else if (registered === false) {
+        go('/not-registered');
+      } else {
+        go('/');
+      }
+    }).catch(e => {
+      this.setState({errors: e})
+    })
   }
 
   setter(name: string) {
@@ -41,8 +61,8 @@ export class Login extends React.Component<LoginProps, {}> {
       <MainContentWrapper>
         <div {...style.box}>
           <div {...style.maxWidth}>
-            <h1 {...style.heading}>Welcome Back</h1>
-            <p>Let us send you an email to return to your account.</p>
+            <h1 {...style.heading}>Welcome Back!</h1>
+            <p>Let us send you an email containing a link to get you back into to your account.</p>
             <ShortInput label="Email" onChange={this.setter('email')} placeholder="you@yourdomain.com" value={this.state.email}/>
             <Button action={this.submit} css={style.button}> Send me a link!</Button>
             <Link to='/'>I still need an account.</Link>
