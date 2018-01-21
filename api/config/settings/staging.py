864b4088f85764740028b0c04529b37547bcdf3f
Jace Browning <jacebrowning@gmail.com>
2018-01-21 14:26:50 -0500

    Save emails to file unless SendGrid credentials are set

--- api/config/settings/staging.py
+++ api/config/settings/staging.py
@@ -7,11 +7,24 @@ BASE_NAME = os.environ['HEROKU_APP_NAME']
 BASE_DOMAIN = f"{BASE_NAME}.herokuapp.com"
 BASE_URL = f"https://{BASE_DOMAIN}"
 
+###############################################################################
+# Core
+
 ALLOWED_HOSTS += [
     '.herokuapp.com',
 ]
 
+###############################################################################
+# Email
+
 DEFAULT_FROM_EMAIL = f"Voter Engagement {BASE_NAME} <noreply@{BASE_DOMAIN}>"
 
-ROLLBAR['environment'] = os.getenv('ROLLBAR_ENVIRONMENT', 'staging')
+###############################################################################
+# Logging
+
 LOGGING['handlers']['rollbar']['environment'] = os.getenv('ROLLBAR_ENVIRONMENT', 'staging')
+
+###############################################################################
+# Rollbar
+
+ROLLBAR['environment'] = os.getenv('ROLLBAR_ENVIRONMENT', 'staging')
