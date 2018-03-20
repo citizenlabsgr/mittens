2d0b44d65a8167a105cbc63e704735b1c360e0c4
Jace Browning <jacebrowning@gmail.com>
2018-03-20 19:40:16 -0400

    Handle logout on the backend

--- api/core/urls.py
+++ api/core/urls.py
@@ -1,11 +1,13 @@
-from django.urls import path, re_path
-from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib.auth.views import logout
+from django.urls import path, re_path
 
 from . import views
 
 
 urlpatterns = static('/compiled/', document_root=settings.BUILD_ROOT) + [
     path('go/<path:path>', views.redirector, name='redirector'),
+    path('logout', logout, {'next_page': '/'}),
     re_path('^', views.index, name='index'),
 ]
