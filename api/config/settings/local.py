f6bd02cda160bafaf67bec9f05757429d3411fd7
Jace Browning <jacebrowning@gmail.com>
2018-03-20 18:57:08 -0400

    Create directory for local emails

--- api/config/settings/local.py
+++ api/config/settings/local.py
@@ -45,3 +45,4 @@ DEFAULT_FROM_EMAIL = f"Voter Engagement {BASE_NAME} <noreply@{BASE_DOMAIN}>"
 if not (os.getenv('SENDGRID_USERNAME') and os.getenv('SENDGRID_PASSWORD')):
     EMAIL_BACKEND = 'django.core.mail.backends.filebased.EmailBackend'
     EMAIL_FILE_PATH = os.path.join(PROJECT_ROOT, 'tmp', 'emails')
+    os.makedirs(EMAIL_FILE_PATH, exist_ok=True)
