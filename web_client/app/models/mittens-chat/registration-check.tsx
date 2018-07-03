9d5461a34b5bef3092f4db62ce7bb2a850ab9059
Aaron King <aaron.king@atomicobject.com>
2018-07-03 19:28:43 -0400

    Remove first yes-or-no prompt

--- web_client/app/models/mittens-chat/registration-check.tsx
+++ web_client/app/models/mittens-chat/registration-check.tsx
@@ -11,24 +11,6 @@ MittensChat.createGoal("registration-check").exchanges = [
       "I'm just a dog, but I'll do my best to help you vote.",
       "It's your duty!",
       "First, let's check if you're registered to vote.",
-      "Have you voted in Michigan before?"
-    ],
-    {
-      options: [{ text: "Yes", value: true }, { text: "No", value: false }]
-      // TODO text-input affirmative
-    },
-    (state, votedBefore) => {
-      Voter.currentUser.votedBefore = votedBefore;
-      MittensChat.changeState({
-        goalName: state.goalName,
-        exchange: (state.exchange + 1)
-      });
-    }
-  ),
-
-  new Exchange(
-    [
-      "OK! I can ask the secretary of state if you're already registered",
       "I'll need a little information about you."
     ],
     {
